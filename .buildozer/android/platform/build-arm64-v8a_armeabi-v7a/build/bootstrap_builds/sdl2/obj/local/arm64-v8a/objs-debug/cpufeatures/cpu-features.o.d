4c76262ce46ed8b4c045600147175d3dcee59387
Gabriel Schvartz <gabriel.schvartz@gmail.com>
2026-06-07 14:03:14 +0000

    Eliminar .buildozer del repo y agregar .gitignore

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,4 +0,0 @@
-/home/gabriel/colnect_proy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/gabriel/.buildozer/android/platform/android-ndk-r28c/sources/android/cpufeatures/cpu-features.c \
-  /home/gabriel/.buildozer/android/platform/android-ndk-r28c/sources/android/cpufeatures/cpu-features.h
-/home/gabriel/.buildozer/android/platform/android-ndk-r28c/sources/android/cpufeatures/cpu-features.h:
